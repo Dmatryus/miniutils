e3633195f706ad5bcc9529ac37c28615a3c035dc
dmatryus <dmatryus.sqrt49@yandex.ru>
2024-02-05 10:28:17 +0300

    Add invert_img.py

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # miniutils
 A set of small scripts for simple tasks.
+
+# Image
+A set of scripts for manipulating images.
+
+## invert_img.py
+Inverts the color of an image.
+Usage: `python invert_img.py -p <path_to_image>`
+
+Inverted image will be saved in the same directory with the same name and the suffix '_neg'.
---
+++ image/invert_img.py
@@ -0,0 +1,18 @@
+import argparse
+
+from pathlib import Path
+from PIL import Image
+import PIL.ImageOps
+
+parser = argparse.ArgumentParser(description="Invert colors of image.")
+parser.add_argument("--path", "-p", type=str, required=True, help="path to image")
+args = parser.parse_args()
+
+
+target_path = Path(sys.argv[1])
+new_path = target_path.parent / f"{target_path.stem}_neg{target_path.suffix}"
+
+image = Image.open(target_path)
+inverted_image = PIL.ImageOps.invert(image)
+
+inverted_image.save(new_path)
